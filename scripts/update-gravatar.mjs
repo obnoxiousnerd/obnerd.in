6d2fdbe2ec633eafdec95abb5132c0d99245feca
Pranav Karawale <pranav@karawale.in>
2022-03-19 13:12:43 +0530

    Fixing webmentions * Auto download Gravatar * h-card in every post

---
+++ scripts/update-gravatar.mjs
@@ -0,0 +1,21 @@
+import crypto from "crypto";
+import Image from "@11ty/eleventy-img";
+
+function constructUrl(email, size) {
+	return `https://secure.gravatar.com/avatar/${md5(email)}?s=${size}`;
+}
+
+function md5(email) {
+	return crypto.createHash("md5").update(email).digest("hex");
+}
+
+const stats = await Image(constructUrl("pranav@karawale.in", 512), {
+	formats: ["webp"],
+	widths: [96, 256],
+	urlPath: "/img/",
+	outputDir: "_assets/img/",
+	filenameFormat: (_id, _src, width, format, _options) =>
+		`me${width}.${format}`,
+});
+
+console.log(stats);
