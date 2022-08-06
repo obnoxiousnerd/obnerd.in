6c147ba7d8bc94a95118f2ee3c9625b4ce72ff43
Pranav Karawale <pranav@karawale.in>
2022-08-06 12:11:39 +0530

    Don't fail if webmentions can't be fetched

--- _data/webmentions.js
+++ _data/webmentions.js
@@ -5,24 +5,28 @@ const { AssetCache } = require('@11ty/eleventy-fetch');
 dotenv.config();
 
 module.exports = async () => {
-  const asset = new AssetCache('webmentions');
-  const url = `https://webmention.io/api/mentions.jf2?token=${process.env.WEBMENTION_IO_TOKEN}&per-page=10000`;
+  try {
+    const asset = new AssetCache('webmentions');
+    const url = `https://webmention.io/api/mentions.jf2?token=${process.env.WEBMENTION_IO_TOKEN}&per-page=10000`;
 
-  if (asset.isCacheValid('4h')) {
-    return asset.getCachedValue();
+    if (asset.isCacheValid('4h')) {
+      return asset.getCachedValue();
+    }
+
+    const req = await fetch(url, {
+      duration: '4h',
+      type: 'json',
+    });
+
+    if (!req.ok) {
+      // Probably a bad idea
+      throw new Error(`Request returned ${req.status}`);
+    }
+
+    const mentions = await req.json();
+    await asset.save(mentions, 'json');
+    return mentions;
+  } catch (e) {
+    return null;
   }
-
-  const req = await fetch(url, {
-    duration: '4h',
-    type: 'json',
-  });
-
-  if (!req.ok) {
-    // Probably a bad idea
-    throw new Error(`Request returned ${req.status}`);
-  }
-
-  const mentions = await req.json();
-  await asset.save(mentions, 'json');
-  return mentions;
 };
